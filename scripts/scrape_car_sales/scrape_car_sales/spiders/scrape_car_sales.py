dd77ee975ecac18db83abf8ac6dcbf458016ba05
Laura Gutierrez Funderburk <laura_gutierrez_funderburk@sfu.ca>
2022-11-01 20:38:46 -0700

    fix data json

--- scripts/scrape_car_sales/scrape_car_sales/spiders/scrape_car_sales.py
+++ scripts/scrape_car_sales/scrape_car_sales/spiders/scrape_car_sales.py
@@ -3,25 +3,25 @@ import scrapy
 class CarSalesSpider(scrapy.Spider):
     name = "car-sales"
 
-    start_urls = ['https://www.goodcarbadcar.net/2019-canada-vehicle-sales-figures-by-model/#monthlysales/']
+    start_urls = ['https://www.goodcarbadcar.net/2019-canada-vehicle-sales-figures-by-model/']
     
     def parse(self, response):
         table = response.xpath('//*[@id="table_3"]/tbody')
-        rows = table.xpath('//tr')
+        rows = table.xpath('//tr[contains(@id,"table_3566")]')
 
         for row in rows:
             yield {
-                'model' : row.xpath('td[1]//text()').extract_first(),
-                'jan': row.xpath('td[2]//text()').extract_first(),
-                'feb': row.xpath('td[3]//text()').extract_first(),
-                'mar': row.xpath('td[4]//text()').extract_first(),
-                'apr': row.xpath('td[5]//text()').extract_first(),
-                'may': row.xpath('td[6]//text()').extract_first(),
-                'jun': row.xpath('td[7]//text()').extract_first(),
-                'jul': row.xpath('td[8]//text()').extract_first(),
-                'aug': row.xpath('td[9]//text()').extract_first(),
-                'sep': row.xpath('td[10]//text()').extract_first(),
-                'oct': row.xpath('td[11]//text()').extract_first(),
-                'nov': row.xpath('td[12]//text()').extract_first(),
-                'dec': row.xpath('td[13]//text()').extract_first()
+                'model' : row.xpath('td[1]//text()').get(),
+                'jan': row.xpath('td[2]//text()').get(),
+                'feb': row.xpath('td[3]//text()').get(),
+                'mar': row.xpath('td[4]//text()').get(),
+                'apr': row.xpath('td[5]//text()').get(),
+                'may': row.xpath('td[6]//text()').get(),
+                'jun': row.xpath('td[7]//text()').get(),
+                'jul': row.xpath('td[8]//text()').get(),
+                'aug': row.xpath('td[9]//text()').get(),
+                'sep': row.xpath('td[10]//text()').get(),
+                'oct': row.xpath('td[11]//text()').get(),
+                'nov': row.xpath('td[12]//text()').get(),
+                'dec': row.xpath('td[13]//text()').get()
             }
